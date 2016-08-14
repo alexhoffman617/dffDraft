76041509156894b336e8a81895c61113612c1c26
Alex Hoffman <alexhoffman617@gmail.com>
2016-08-14 19:53:41 -0400

    fixed routerlinkactive issue

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { LoginService } from '../services/login.service';
+
 
 @Component({
     templateUrl: './app/user/user.component.html'
 })
 export class UserComponent{
-    
+    loginService;
+    userBids;
+    constructor(af: AngularFire, loginService: LoginService){
+        this.loginService = loginService;
+
+        if(this.loginService.user && this.loginService.user.userId){
+            this.userBids = af.database.list('bids', {
+                query: {
+                orderByChild: 'username',
+                equalTo: this.loginService.user.userHash
+                }
+            })
+        }
+
+    }
+
 }
