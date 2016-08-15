7d16b03fe073f5209851f9a9d9d0043b561a6905
Alex Hoffman <alexhoffman617@gmail.com>
2016-08-14 21:57:36 -0400

    table css and user table gets player info

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,21 +4,35 @@ import { LoginService } from '../services/login.service';
 
 
 @Component({
-    templateUrl: './app/user/user.component.html'
+    templateUrl: './app/user/user.component.html',
 })
 export class UserComponent{
     loginService;
     userBids;
+    players;
+        getPlayer(playerId){
+        
+    };
     constructor(af: AngularFire, loginService: LoginService){
         this.loginService = loginService;
 
         if(this.loginService.user && this.loginService.user.userId){
+            var allPlayers = af.database.list('player');
+            allPlayers.subscribe(snapshots => {
+                 this.players = snapshots;
+            });
+
             this.userBids = af.database.list('bids', {
                 query: {
                 orderByChild: 'username',
                 equalTo: this.loginService.user.userHash
                 }
             })
+        }
+
+        this.getPlayer = function(playerId){
+            var player = this.players.filter(function(snapshot){ return snapshot.$key == playerId })[0];
+            return player;
         }
 
     }
