cd46043ac0441c3dc84dd21b8263ac2e74ea6b6b
Alex Hoffman <alexhoffman617@gmail.com>
2016-08-16 22:16:42 -0400

    Added bid timekeeping

--- src/app/playerTable/playerTable.component.ts
+++ src/app/playerTable/playerTable.component.ts
@@ -19,7 +19,11 @@ export class PlayerTableComponent  {
   positionFilter;
   usernameFilter;
   loginService;
+  datetime;
   getBid(playerId){};
+  getTimeLeft(timestamp, inputCurrentDateTime){};
+  getTimeFromTimeStamp(timestamp){};
+  formatTimeNumber(input){};
   constructor(af: AngularFire, loginService: LoginService) {
     this.loginService = loginService;
     this.positionFilter = 'ALL';
@@ -48,5 +52,33 @@ export class PlayerTableComponent  {
         }
     }
 
+    this.getTimeLeft = function(inputTimeStamp, inputCurrentDateTime){
+      var inputDateTime = new Date(inputTimeStamp);
+      var endtime = inputDateTime.setDate(inputDateTime.getDate() + 1)
+      var t = endtime - inputCurrentDateTime;
+        var seconds = this.formatTimeNumber(Math.floor( (t/1000) % 60 ));
+        var minutes = this.formatTimeNumber(Math.floor( (t/1000/60) % 60 ));
+        var hours = this.formatTimeNumber(Math.floor( (t/(1000*60*60)) % 24 ));
+        if(t < 0){
+          return 'Bid Won';
+        }
+        return  hours + ':' + minutes + ':' + seconds;
+        ;
+    }
+
+    this.formatTimeNumber = function(input){
+      if(input.toString().length > 1){
+        return input;
+      } else {
+        return "0" + input;
+      }
+    }
+
+    this.getTimeFromTimeStamp = function(timestamp){
+      return new Date(timestamp);
+    }
+        setInterval(() => {
+        this.datetime =  new Date().getTime();
+     }, 1000);
   }
 }
